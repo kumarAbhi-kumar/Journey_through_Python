44bb12cdb0ca11a0c7cd9de63e2c598ee85018fe
KumarAbhi <fabiamkumar@gmail.com>
2023-01-01 23:08:21 +0530

    Variables

---
+++ 3 Variables.py
@@ -0,0 +1,30 @@
+"""Variables:
+These are the named memory location inside python."""
+
+# Named memory location:
+'''1. Named: Variables have a unique name.
+        No two or more variable can have the same name.'''
+'''2. memory location: Variables occupy a particular space in the memory.
+        On creation variables are allocated a particular space in the memory'''
+
+# Naming conventions for variables.
+'''
+Variables names consist of:
+1. Alphabets.
+2. Numbers.
+3. Underscore(_) as the only special character.
+
+Variables name should start with:
+1. Alphabets
+2. underscore(_)
+But not, Numbers.
+'''
+Variable1 = "valid"
+variable1 = "valid"
+_variable1 = "valid"
+Variable_1 = "valid"
+
+# 1_variable = "not valid" as starting with numbers
+# variable@ = "not valid" as contains invalid characters.
+
+# On upcoming lessons we will deal with scope of the variables
