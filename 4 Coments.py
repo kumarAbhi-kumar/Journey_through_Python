0a90e24e3cb714bb9a57191ef5a40a394e42bbba
KumarAbhi <fabiamkumar@gmail.com>
2023-01-02 23:42:20 +0530

    comments

---
+++ 4 Coments.py
@@ -0,0 +1,32 @@
+"""
+Comments:
+>> These are human-readable lines.
+>> Programmers generally use to describe "what's happening" during the code.
+>> These are not executed while the program is executed and only visible to those who has the source code.
+    (Source_code -> The actual program written by the programmer is called Source code.)
+"""
+
+'''
+Types of comments in Python
+1. # -> Single line comment.
+    >> Best suited for describing variables and their uses.
+    >> Functions their applications and a bit specific details about it...
+'''
+
+# This is a single line comment.
+
+'''
+2. Multiline Comment:
+    >> On paper Python doesn't really allows Multi-Line comment.
+    >> But their are multiple ways to overcome this issue.
+'''
+
+'''
+2.1 Using Single-quotes/Double quotes 3-times at the beginning and 3-times at the end.
+'''
+
+"""
+2.2 Using Docstring
+We will discuss while dealing with 'functions'.
+"""
+
